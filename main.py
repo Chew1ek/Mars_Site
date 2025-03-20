268b2871514febe09868a1d964b628d264c3de68
Namilsky <alive6863@gmail.com>
2025-03-20 19:11:25 +0300

    1

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import *
+from data import db_session
+from data.users import User
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('base.html')
+
+@app.route('/test')
+def test():
+    return render_template("content.html")
+
+def main():
+    db_session.global_init("db/blogs.db")
+    app.run(port=8080, host='127.0.0.1')
+
+
+
+
+if __name__ == '__main__':
+    main()
